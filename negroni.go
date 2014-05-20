7cbd1208cd9660d44a13b852ba3ed090fa127845
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-05-20 08:37:38 -0700

    More docs on Handler contracts

--- negroni.go
+++ negroni.go
@@ -10,6 +10,8 @@ import (
 // in the Negroni middleware stack.
 // ServeHTTP should yield to the next middleware in the chain by invoking the next http.HandlerFunc
 // passed in.
+//
+// If the Handler writes to the ResponseWriter, the next http.HandlerFunc should not be invoked.
 type Handler interface {
 	ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc)
 }
