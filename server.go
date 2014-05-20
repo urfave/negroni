8ba5269a3ae34bd62ebfd835901dfc021618e262
saul wiggin <saulwiggin@googlemail.com>
2014-05-20 08:46:18 +0000

    added and run server.go
    set up negroni: go in html

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "github.com/codegangsta/negroni"
+  "net/http"
+  "fmt"
+)
+
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "Welcome to the home page!")
+  })
+
+  n := negroni.Classic()
+  n.UseHandler(mux)
+  n.Run(":3000")
+}
