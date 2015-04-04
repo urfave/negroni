33a94c0ca29593c10e9e4998042dd0ed6c1e1746
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2015-04-04 15:13:40 +0200

    Add TestNoRecovery

--- recovery_test.go
+++ recovery_test.go
@@ -8,6 +8,25 @@ import (
 	"testing"
 )
 
+func TestNoRecovery(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+
+	rec := NewRecovery()
+	rec.Logger = log.New(buff, "[negroni] ", 0)
+
+	n := New()
+	// replace log for testing
+	n.Use(rec)
+	n.UseHandler(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "writing")
+	}))
+	n.ServeHTTP(recorder, (*http.Request)(nil))
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, recorder.Body.Len(), 0)
+	expect(t, len(buff.String()), 0)
+}
+
 func TestRecovery(t *testing.T) {
 	buff := bytes.NewBufferString("")
 	recorder := httptest.NewRecorder()
