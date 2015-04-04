ad161faadd56b6ada76d36e726f39160d88da9d4
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2015-04-04 15:23:11 +0200

    Add TestRecoveryAfterWriting

--- recovery_test.go
+++ recovery_test.go
@@ -45,3 +45,23 @@ func TestRecovery(t *testing.T) {
 	refute(t, recorder.Body.Len(), 0)
 	refute(t, len(buff.String()), 0)
 }
+
+func TestRecoveryAfterWriting(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+
+	rec := NewRecovery()
+	rec.Logger = log.New(buff, "[negroni] ", 0)
+
+	n := New()
+	// replace log for testing
+	n.Use(rec)
+	n.UseHandler(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "writing")
+		panic("here is a panic!")
+	}))
+	n.ServeHTTP(recorder, (*http.Request)(nil))
+	expect(t, recorder.Code, http.StatusInternalServerError)
+	refute(t, recorder.Body.Len(), 0)
+	refute(t, len(buff.String()), 0)
+}
