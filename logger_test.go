4e3a0d8f82348595d942c3c2b01b9ab3ec841bf6
Eivind Siqveland Larsen <to.eivind@gmail.com>
2015-06-20 14:29:26 +0200

    logger: test NewWithLogger(*log.Logger)

--- logger_test.go
+++ logger_test.go
@@ -31,3 +31,27 @@ func Test_Logger(t *testing.T) {
 	expect(t, recorder.Code, http.StatusNotFound)
 	refute(t, len(buff.String()), 0)
 }
+
+func Test_With_Logger(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+
+	applog := log.New(buff, "[negroni] ", 0)
+	l := NewWithLogger(applog)
+
+	n := New()
+	// replace log for testing
+	n.Use(l)
+	n.UseHandler(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusNotFound)
+	}))
+
+	req, err := http.NewRequest("GET", "http://localhost:3000/foobar", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	n.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusNotFound)
+	refute(t, len(buff.String()), 0)
+}
