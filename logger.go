f9817376b85e19cdafe78f4295649245f28c1f2a
Yannick Heinrich <yannick.heinrich@gmail.com>
2016-12-23 12:53:45 +0100

    Use a template to define the logging output.

--- logger.go
+++ logger.go
@@ -1,11 +1,35 @@
 package negroni
 
 import (
+	"bytes"
+
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"text/template"
 	"time"
 )
+
+// Log is the structure
+// passed to the template.
+type Log struct {
+	StartTime string
+	Status    int
+	Duration  time.Duration
+	Hostname  string
+	Method    string
+	Path      string
+}
+
+// DefaultFormat is the format
+// logged used by the default Logger instance.
+var DefaultFormat = "{{.StartTime}} | {{.Status}} | \t {{.Duration}} | {{.Hostname}} | {{.Method}} {{.Path}} \n"
+
+// DefaultDateFormat is the
+// format used for date by the
+// default Logger instance.
+var DefaultDateFormat = time.RFC3339
 
 // ALogger interface
 type ALogger interface {
@@ -17,11 +41,25 @@ type ALogger interface {
 type Logger struct {
 	// ALogger implements just enough log.Logger interface to be compatible with other implementations
 	ALogger
+	dateFormat string
+	template   *template.Template
+	buffer     *bytes.Buffer
+	mu         sync.Mutex
 }
 
 // NewLogger returns a new Logger instance
 func NewLogger() *Logger {
-	return &Logger{log.New(os.Stdout, "[negroni] ", 0)}
+	logger := &Logger{ALogger: log.New(os.Stdout, "[negroni] ", 0), dateFormat: DefaultDateFormat, buffer: bytes.NewBufferString("")}
+	logger.SetFormat(DefaultFormat)
+	return logger
+}
+
+func (l *Logger) SetFormat(format string) {
+	l.template = template.Must(template.New("negroni_parser").Parse(format))
+}
+
+func (l *Logger) SetDateFormat(format string) {
+	l.dateFormat = format
 }
 
 func (l *Logger) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
@@ -30,5 +68,11 @@ func (l *Logger) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.Ha
 	next(rw, r)
 
 	res := rw.(ResponseWriter)
-	l.Printf("%v | %v | \t %v | %v | %v %v", start.Format(time.RFC3339), res.Status(), time.Since(start), r.Host, r.Method, r.URL.Path)
+	log := Log{start.Format(l.dateFormat), res.Status(), time.Since(start), r.Host, r.Method, r.URL.Path}
+
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.template.Execute(l.buffer, log)
+	l.Printf(l.buffer.String())
+	l.buffer.Reset()
 }
